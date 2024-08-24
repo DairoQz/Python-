71f9e3b9f3df4fd51d93530154eadf7d15f62234
DairoQz <dan.ro11@hotmail.com>
2024-08-24 01:24:30 -0600

    Create 05_letter.py

---
+++ 1-hello-world/05_letter.py
@@ -0,0 +1,9 @@
+# Camara Vato
+# Eres Hermoso
+print('*****************************')
+print('*                           *')
+print('*   Recorda sos hermoso     *')
+print('*  Te lo guro por           *')
+print('* Dieguito Armando Maradona *')
+print('*               Dan👁       *')
+print('*****************************')
