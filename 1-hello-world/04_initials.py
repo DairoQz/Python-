62108961798832e3fade5875767a14aa4174081a
DairoQz <dan.ro11@hotmail.com>
2024-08-24 01:15:05 -0600

    Create 04_initials.py

---
+++ 1-hello-world/04_initials.py
@@ -0,0 +1,9 @@
+# I have 18 years
+
+print ('DDDD   RRRRR ')
+print ('D   D  R   RR')
+print ('D   D  RRRRR ')
+print ('D   D  RR    ')
+print ('D   D  R R   ')
+print ('D   D  R  R  ')
+print ('DDDD   R   R ')
