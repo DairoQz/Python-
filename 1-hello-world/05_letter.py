f669e761521b789c00231c38dc53b648738f8d59
DairoQz <dan.ro11@hotmail.com>
2024-08-24 01:25:16 -0600

    Update 05_letter.py

--- 1-hello-world/05_letter.py
+++ 1-hello-world/05_letter.py
@@ -3,7 +3,7 @@
 print('*****************************')
 print('*                           *')
 print('*   Recorda sos hermoso     *')
-print('*  Te lo guro por           *')
+print('*  Te lo juro por           *')
 print('* Dieguito Armando Maradona *')
 print('*               Dan👁       *')
 print('*****************************')
