3cbd1616e231a9a3698f196c1b219f23cc41587b
DairoQz <dan.ro11@hotmail.com>
2024-08-27 01:08:25 -0600

    Create Moneda.py

---
+++ 1-hello-world/Moneda.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+a = int(input('What do you have left in pesos? = '))
+
+b = int(input('What do you have left in soles? = '))
+
+c = int(input('What do you have left in reais? = '))
+
+d = ( a * 0.00025 +  b * 0.27 + c * 0.18 )
+
+print (d)
