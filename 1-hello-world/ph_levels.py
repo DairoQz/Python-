f52bdfa06ae6ae23753dc3cd398fe978bc0f9eb7
DairoQz <dan.ro11@hotmail.com>
2024-08-30 23:48:10 -0600

    Create  ph_levels.py

---
+++ 1-hello-world/ph_levels.py
@@ -0,0 +1,13 @@
+# Write code below 💖
+
+# pH Levels 🧪
+# Codédex
+
+ph = int(input('Enter a pH level (0-14): '))
+
+if ph > 7:
+  print('Basic')
+elif ph < 7:
+  print('Acidic')
+else:
+  print('Neutral')
