63f16d970b5c3bae88955859d212a6047fd083fb
DairoQz <dan.ro11@hotmail.com>
2024-09-02 00:52:55 -0600

    Create 99 bottlesof beer.py

---
+++ 1-hello-world/99 bottlesof beer.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+# String interpolation
+
+for x in range(99, 0, -1):
+  print(f'''{x} bootles of beer on the wall
+{x} bootles of beer 
+Take one down, pass it around
+  ''')
